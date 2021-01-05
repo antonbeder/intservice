1a7dba694fbf1383c2de4210eba8863c10582073
antonbeder <bederanton@gmail.com>
2021-01-05 11:29:04 +0200

    13

--- intservice/tests/basic.test.sh
+++ intservice/tests/basic.test.sh
@@ -12,8 +12,14 @@ check_result () {
   fi
 }
 
+containerName=$(echo $TAG | cut -d'-' -f2)
+
+
+sudo docker rm -f $(sudo docker ps -aq)
+sudo docker run -d -p 5000:5000 --name $containerName $TAG
+sleep 5
 for sentiment in positive negative neutral; do
-  sudo docker run $TAG -w $sentiment | grep $sentiment
+  curl -s -X POST --header "Content-Type: application/json" --data '{"word":"'$sentiment'"}' http://localhost:5000 | grep $sentiment
   check_result $sentiment
 done
 
